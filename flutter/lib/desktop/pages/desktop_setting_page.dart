5992c12605799f261ac9946d85fb1eb1ee6c146b
Lilianne_Blaze <39486911+Lilianne-Blaze@users.noreply.github.com>
2026-09-21 04:04:59 +0200

    Mark WinPE limited edition in version label
    Update the desktop About page to append the limited edition mod1-winpe suffix to the displayed version string so WinPE builds are clearly identified in the UI.

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2437,7 +2437,7 @@ class _AboutState extends State<_About> {
                 height: 8.0,
               ),
               SelectionArea(
-                  child: Text('${translate('Version')}: $version')
+                  child: Text('${translate('Version')}: $version limited edition mod1-winpe')
                       .marginSymmetric(vertical: 4.0)),
               SelectionArea(
                   child: Text('${translate('Build Date')}: $buildDate')
