325eaf7cd1cfd442520a47616c7457d9cfa108e6
Lilianne_Blaze <39486911+Lilianne-Blaze@users.noreply.github.com>
2026-09-21 05:21:32 +0200

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2437,7 +2437,7 @@ class _AboutState extends State<_About> {
                 height: 8.0,
               ),
               SelectionArea(
-                  child: Text('${translate('Version')}: $version limited edition mod1-winpe')
+                  child: Text('${translate('Version')}: $version limited edition mod2-winpe')
                       .marginSymmetric(vertical: 4.0)),
               SelectionArea(
                   child: Text('${translate('Build Date')}: $buildDate')
