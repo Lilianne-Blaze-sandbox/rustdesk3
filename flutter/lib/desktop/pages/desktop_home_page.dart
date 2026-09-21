e5a0b43b8b09c9d9b108a5df8723f6544ab0706d
Lilianne_Blaze <39486911+Lilianne-Blaze@users.noreply.github.com>
2026-09-21 05:26:36 +0200

    Adjust desktop home layout for longer IDs
    Increase the non-incoming desktop home panel width to give longer IDs more room, and slightly reduce the ID label font size to keep the layout balanced.

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -131,7 +131,9 @@ class _DesktopHomePageState extends State<DesktopHomePage>
     return ChangeNotifierProvider.value(
       value: gFFI.serverModel,
       child: Container(
-        width: isIncomingOnly ? 280.0 : 200.0,
+        //width: isIncomingOnly ? 280.0 : 200.0,
+        // support longer ids
+        width: isIncomingOnly ? 280.0 : 220.0,
         color: Theme.of(context).colorScheme.background,
         child: Stack(
           children: [
@@ -215,7 +217,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
                         Text(
                           translate("ID"),
                           style: TextStyle(
-                              fontSize: 14,
+                              fontSize: 12,
                               color: Theme.of(context)
                                   .textTheme
                                   .titleLarge
